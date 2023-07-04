ff2dd9bec40464d7f5be8f5249a01864c78be278
Shadab Khan <shadabkhan7253@gmail.com>
2023-07-04 20:51:01 +0530

    relationship with product added

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Seller extends Model
+class Seller extends User
 {
     use HasFactory;
+
+    protected $table = 'users';
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
