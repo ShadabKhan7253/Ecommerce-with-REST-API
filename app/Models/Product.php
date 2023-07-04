1cee1225a3f71aa092d311de77b725ffefab5f7c
Shadab Khan <shadabkhan7253@gmail.com>
2023-07-04 20:50:32 +0530

    relationship with category, seller and transaction added

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,35 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    const UNAVAILABLE_PRODUCT = "unavailable";
+    const AVAILABLE_PRODUCT = "available";
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id'
+    ];
+
+    public function isAvailable(): bool
+    {
+        return $this->status === self::AVAILABLE_PRODUCT;
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+
+    public function seller()
+    {
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
