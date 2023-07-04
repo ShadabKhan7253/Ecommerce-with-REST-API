4461166d89d00812551be31830656d4c52709fdc
Shadab Khan <shadabkhan7253@gmail.com>
2023-07-04 20:53:54 +0530

    Route added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\User\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::resource('buyers', \App\Http\Controllers\Buyer\BuyersController::class, ['only'=>['index', 'show']]);
+Route::resource('users', UsersController::class, ['except'=>['create', 'edit']]);
