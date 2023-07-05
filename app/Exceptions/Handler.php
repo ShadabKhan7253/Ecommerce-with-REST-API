29bddd91aa67fc6f922e83417c0e5163956c5599
Shadab Khan <shadabkhan7253@gmail.com>
2023-07-05 12:14:28 +0530

    handle exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,21 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
+use Dotenv\Exception\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException as ValidationValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of exception types with their corresponding custom log levels.
      *
@@ -46,5 +56,49 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if($e instanceof ValidationValidationException) {
+            return $this->convertValidationExceptionToResponse($e, $request);
+        }
+
+        if($e instanceof AuthenticationException) {
+            return $this->unauthenticated($request,$e);
+        }
+
+        if($e instanceof AuthorizationException) {
+            return $this->errorResponse($e->getMessage(),403);
+        }
+
+        if($e instanceof NotFoundHttpException) {
+            return $this->errorResponse("The specified URL cannot be found",404);
+        }
+
+        if($e instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse("The specified method is invalid for this request",405);
+        }
+
+        if($e instanceof ModelNotFoundException) {
+            $modelName = class_basename($e->getModel());
+            return $this->errorResponse("$modelName does not exist with the specified key",404);
+        }
+
+        if($e instanceof HttpException) {
+            return $this->errorResponse($e->getMessage(),$e->getStatusCode());
+        }
+        return parent::render($request,$e);
+
+    }
+
+    protected function convertValidationExceptionToResponse(ValidationValidationException $e, $request)
+    {
+        $this->errorResponse($e->getMessage(),422);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse("Unauthenticated",401);
     }
 }
