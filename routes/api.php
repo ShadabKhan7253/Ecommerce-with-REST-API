598e12de711fb17fc3f03edf2f033d360ccacda9
Shadab Khan <shadabkhan7253@gmail.com>
2023-07-05 13:10:10 +0530

    new route added

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Category\CategoriesController;
+use App\Http\Controllers\Product\ProductsController;
 use App\Http\Controllers\Seller\SellersController;
+use App\Http\Controllers\Transaction\TransactionsController;
 use App\Http\Controllers\User\UsersController;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +23,9 @@ use Illuminate\Support\Facades\Route;
 Route::resource('users', UsersController::class, ['except'=>['create', 'edit']]);
 Route::resource('buyers', \App\Http\Controllers\Buyer\BuyersController::class, ['only'=>['index', 'show']]);
 Route::resource('sellers', SellersController::class, ['only'=>['index', 'show']]);
+Route::resource('categories', CategoriesController::class, ['except'=>['create', 'edit']]);
+Route::resource('products', ProductsController::class, ['only'=>['index', 'show']]);
+Route::resource('transactions', TransactionsController::class, ['only'=>['index', 'show']]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
