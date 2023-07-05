0c95660550d614390b420ead25af93c7c8363662
Shadab Khan <shadabkhan7253@gmail.com>
2023-07-05 13:09:22 +0530

    boot method added

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\BuyerScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,6 +10,12 @@ class Buyer extends User
 {
     use HasFactory;
     protected $table = 'users';
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new BuyerScope());
+    }
 
     public function transactions()
     {
--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -2,14 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\SellerScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Seller extends User
 {
     use HasFactory;
 
     protected $table = 'users';
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new SellerScope());
+    }
 
     public function products()
     {
