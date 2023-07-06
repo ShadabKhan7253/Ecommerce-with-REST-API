627e5e331f59989165486725dcd55b26367272f5
Shadab Khan <shadabkhan7253@gmail.com>
2023-07-06 11:49:57 +0530

    Nested seller controller added

---
+++ app/Http/Controllers/Seller/SellerTransactionController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
+use App\Models\Seller;
+use Illuminate\Http\Request;
+
+class SellerTransactionController extends ApiController
+{
+    public function index(Seller $seller)
+    {
+        $transactions = $seller->products()
+            ->whereHas('transactions')
+            ->with('transactions')
+            ->get()
+            ->pluck('transactions')
+            ->flatten();
+
+            return $this->showAll($transactions);
+    }
+}
